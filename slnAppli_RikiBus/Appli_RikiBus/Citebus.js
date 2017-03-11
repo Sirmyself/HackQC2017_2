a6fc0adf122fe18e415cd4591df32d8e1f9f544c
MasterWaldo42 <charlesn_pinel@hotmail.com>
2017-03-10 22:53:53 -0500

    Sync Citebus.js

--- slnAppli_RikiBus/Appli_RikiBus/Citebus.js
+++ slnAppli_RikiBus/Appli_RikiBus/Citebus.js
@@ -1,18 +1,20 @@
-﻿var mapCitebus = L.map('map').setView(-68.5289754901558, 48.4506343914947, 13);
+﻿$('document').ready(function () {
+    var mapCitebus = L.map('map').setView(-68.5289754901558, 48.4506343914947, 13);
 
-//Carte modiale de base
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
-}).addTo(mapCitebus);
+    //Carte modiale de base
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'your.mapbox.project.id',
+        accessToken: 'your.mapbox.public.access.token'
+    }).addTo(mapCitebus);
 
 
-//Loading du GeoJson
-var layerArretcitebus = L.geoJSON().addTo(mapCitebus);
+    //Loading du GeoJson
+    var layerArretcitebus = L.geoJSON().addTo(mapCitebus);
 
-$.getJSON("arretcitebus.json", function (json) {
-    layerArretcitebus.addData(json);
+    $.getJSON("arretcitebus.json", function (json) {
+        layerArretcitebus.addData(json);
 
+    });
 });
